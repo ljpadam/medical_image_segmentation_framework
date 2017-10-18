c1c4242924fa476f9d3852b4d0f8d3451a5ee56a
ljpadam <ljpadam@gmail.com>
2017-10-18 17:22:34 +0800

    fix bugs of focal loss

--- FocalLoss.py
+++ FocalLoss.py
@@ -4,24 +4,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
-    r"""
-        This criterion is a implemenation of Focal Loss, which is proposed in 
-        Focal Loss for Dense Object Detection.
 
-            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
-
-        The losses are averaged across observations for each minibatch.
-
-        Args:
-            alpha(1D Tensor, Variable) : the scalar factor for this criterion
-            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
-                                   putting more focus on hard, misclassiﬁed examples
-            size_average(bool): By default, the losses are averaged over observations for each minibatch.
-                                However, if the field size_average is set to False, the losses are
-                                instead summed for each minibatch.
-
-
-    """
     def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
         super(FocalLoss, self).__init__()
         if alpha is None:
@@ -44,7 +27,8 @@ class FocalLoss(nn.Module):
         class_mask = inputs.data.new(N, C).fill_(0)
         class_mask = Variable(class_mask)
         ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
+        ones = Variable(torch.Tensor(ids.size()).fill_(1)).cuda()
+        class_mask.scatter_(1, ids, ones)
         #print(class_mask)
 
 
@@ -54,7 +38,7 @@ class FocalLoss(nn.Module):
 
         probs = (P*class_mask).sum(1).view(-1,1)
 
-        log_p = probs.log()
+        log_p = probs
         #print('probs size= {}'.format(probs.size()))
         #print(probs)
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # params of the algorithm
 
     params['ModelParams']['device'] = 0 # the id of the GPU
-    params['ModelParams']['snapshot'] = 114000 #85000
+    params['ModelParams']['snapshot'] = 0 #85000
     params['ModelParams']['dirTrain'] = '/home/ljp/data/lung/manual/randomcrops/training' # the directory of training data
     #params['ModelParams']['dirTest'] = '/home/ljp/from_Dfwang/WML/testing'
     # where we need to save the results (relative to the base path)
